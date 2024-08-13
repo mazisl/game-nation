06164b1ae90050952d40e419818b55f9622dc92d
Maz <maz.islam1@gmail.com>
2024-08-13 09:39:40 +0400

    optimized genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -33,22 +33,25 @@ const GenreList = ({onSelectGenre, selectedGenreText}: GenreListProps) => {
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
   return (
-    <ul>
-      {isLoading && (
-        skeletons.map(skeleton => (
-          <li key={skeleton} className="py-[5px]"><GenreListSkeleton /></li>
-        ))
-      )}
-      {data.map(genre => {
-        const fontWeight = genre.id === selectedGenreText?.id ? 'font-bold' : 'font-normal';
-        return (
-          <li key={genre.id} className="flex py-[5px] items-center">
-            <img src={getCroppedImageUrl(genre.image_background)} className="h-8 w-8 rounded-lg" />
-            <Button className={`text-base ${fontWeight}`} variant='link' onClick={() => onSelectGenre(genre)}>{genre.name}</Button>
-          </li>
-        )
-      })}
-    </ul>
+    <>
+      <h1 className="text-2xl font-semibold mb-4">Genres</h1>
+      <ul>
+        {isLoading && (
+          skeletons.map(skeleton => (
+            <li key={skeleton} className="py-[5px]"><GenreListSkeleton /></li>
+          ))
+        )}
+        {data.map(genre => {
+          const fontWeight = genre.id === selectedGenreText?.id ? 'font-bold' : 'font-normal';
+          return (
+            <li key={genre.id} className="flex py-[5px] items-center">
+              <img src={getCroppedImageUrl(genre.image_background)} className="h-8 w-8 rounded-lg object-cover" />
+              <Button className={`text-base ${fontWeight} whitespace-normal text-left`} variant='link' onClick={() => onSelectGenre(genre)}>{genre.name}</Button>
+            </li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
 
