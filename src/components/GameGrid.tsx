b7223ec71a05fabee7fbc4ed62d8665d95db0079
Maz <maz.islam1@gmail.com>
2024-08-15 13:38:50 +0400

    added infinite scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,7 +5,9 @@ import GameCardContainer from "./GameCardContainer";
 // import { Genre } from "@/hooks/useGenres";
 import { GameQuery } from "@/App";
 import React from "react";
-import { Button } from "./ui/button";
+// import { Button } from "./ui/button";
+import { Progress } from "./ui/progress";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface GameGridProps {
   gameQuery: GameQuery;
@@ -13,38 +15,42 @@ interface GameGridProps {
 
 const GameGrid = ({gameQuery}: GameGridProps) => {
   
-  const {data, error, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage} = useGames(gameQuery);
+  const {data, error, isLoading, fetchNextPage, hasNextPage} = useGames(gameQuery);
 
   const skeletons = [1, 2, 3, 4, 5, 6];
   
   if (error) return <p>{error.message}</p>;
 
+  const fetchedGamesCount = data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
+
   return (
     <>
-      <div className="flex justify-center">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
-          {isLoading && skeletons.map((skeleton => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          )))}
-          {/* {data?.results.map((game) => (
-            <GameCardContainer key={game.id}>
-              <GameCard game={game} />
-            </GameCardContainer>
-          ))} */}
-          {/* infinite queries */}
-          {data?.pages.map((page, index) => 
-            <React.Fragment key={index}>
-              {page.results.map(game => (
-                <GameCardContainer key={game.id}>
-                  <GameCard game={game} />
-                </GameCardContainer>
-              ))}
-            </React.Fragment>)}
+      <InfiniteScroll dataLength={fetchedGamesCount} hasMore={!!hasNextPage} next={() => fetchNextPage()} loader={<Progress value={45} />}>
+        <div className="flex justify-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+            {isLoading && skeletons.map((skeleton => (
+              <GameCardContainer key={skeleton}>
+                <GameCardSkeleton />
+              </GameCardContainer>
+            )))}
+            {/* {data?.results.map((game) => (
+              <GameCardContainer key={game.id}>
+                <GameCard game={game} />
+              </GameCardContainer>
+            ))} */}
+            {/* infinite queries */}
+            {data?.pages.map((page, index) => 
+              <React.Fragment key={index}>
+                {page.results.map(game => (
+                  <GameCardContainer key={game.id}>
+                    <GameCard game={game} />
+                  </GameCardContainer>
+                ))}
+              </React.Fragment>)}
+          </div>
         </div>
-      </div>
-      {hasNextPage && <Button onClick={() => fetchNextPage()} className="my-5 ml-4">{isFetchingNextPage ? 'Loading...' : 'Load More'}</Button>}
+      </InfiniteScroll>
+      {/* {hasNextPage && <Button onClick={() => fetchNextPage()} className="my-5 ml-4">{isFetchingNextPage ? 'Loading...' : 'Load More'}</Button>} */}
     </>
     
   )
