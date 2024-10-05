190be24019415b28887531f1dc4644e3ed08b2ad
Maz <maz.islam1@gmail.com>
2024-10-05 17:08:08 +0400

    revert badge to original color

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -6,7 +6,7 @@ interface CriticScoreProps {
 
 const CriticScore = ({score}: CriticScoreProps) => {
 
-  const color = score > 75 ? 'bg-green-400' : score > 60 ? 'bg-yellow-300' : '';
+  const color = score > 75 ? 'bg-green-300' : score > 60 ? 'bg-yellow-300' : '';
 
   return (
     <Badge className={color}>{score}</Badge>
